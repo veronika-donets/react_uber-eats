886ac92b9c7c9d14316bdebd4bb7a26a4a662b51
Veronika D <donets.veronika@yahoo.com>
2019-11-27 11:00:45 +0200

    Updates

---
+++ precache-manifest.2645efe50669a3463d245fe0e03cf571.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c0470266c518ce7696839bd727be3d37",
+    "url": "/react_uber-eats/index.html"
+  },
+  {
+    "revision": "b2125e8541f097f8ee7d",
+    "url": "/react_uber-eats/static/css/main.4bbaddda.chunk.css"
+  },
+  {
+    "revision": "d0e798791a29063a09de",
+    "url": "/react_uber-eats/static/js/2.090ab42a.chunk.js"
+  },
+  {
+    "revision": "b2125e8541f097f8ee7d",
+    "url": "/react_uber-eats/static/js/main.91d691c5.chunk.js"
+  },
+  {
+    "revision": "183d5977488a46d51899",
+    "url": "/react_uber-eats/static/js/runtime~main.bef14170.js"
+  }
+]);
